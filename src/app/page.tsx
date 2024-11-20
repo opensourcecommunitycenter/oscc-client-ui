6ff93c81230dcfaffe2cd06224496714532786f3
Shaikh Mohammad Umar Farooq <shaikhumar06122001@gmail.com>
2024-11-20 22:03:49 +0530

    feat(project-setup): add shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
     return (
         <main>
             <h1>Welcome to OSCC</h1>
+            <Button variant="default">Click Me!</Button>
         </main>
     );
 }
